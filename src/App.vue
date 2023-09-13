2f4081813eb7604cb921ce94074b309c77f3bb4a
JashwandAutar <jashwandautar@gmail.com>
2023-09-13 13:38:34 +0200

    Hello World

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    
+    <img alt="Vue logo" class="logo" src="./assets/logo REC.png"  />
+    
+  
+  </header>
+
+  <main>
+    <HelloWorld msg="Welkom bij Real Estate Care" />
+  </main>
+</template>
+
+<style scoped>
+  
+  .logo{
+    width: 15rem;
+  }
+
+  header{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  main{
+    background-image: linear-gradient(50deg, var(--teal) 58%, var(--black) 72%);
+  }
+</style>
