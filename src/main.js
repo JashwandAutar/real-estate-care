4f35e5f7c13ff52331fad7d9b6b5cdd6b2500134
JashwandAutar <jashwandautar@gmail.com>
2023-11-01 11:06:34 +0100

    Installed Primevue and theme.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-//import Primevue from 'primevue/config'
+import Primevue from 'primevue/config'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Primevue).mount('#app')
