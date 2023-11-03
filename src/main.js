2486bf6f0d3232e8fc6b3653bdb94d890e8c68ba
JashwandAutar <jashwandautar@gmail.com>
2023-11-03 12:06:12 +0100

    Main.JS reworked

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
-import './assets/main.css'
-import './assets/theme.css'
-import 'primevue/resources/primevue.min.css'
+import './assets/main.css';
+import './assets/theme.css';
+import 'primevue/resources/primevue.min.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Primevue from 'primevue/config'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import Primevue from 'primevue/config';
 
 import SelectButton from 'primevue/selectbutton';
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
+import Dropdown from 'primevue/dropdown';
+import RadioButton from 'primevue/radiobutton';
+import Checkbox from 'primevue/checkbox';
+import Button from 'primevue/button';
 
-createApp(App).use(router).use(Primevue).component('SelectButton', SelectButton).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(Primevue);
+
+app.component('SelectButton', SelectButton);
+app.component('InputText', InputText);
+app.component('Textarea', Textarea);
+app.component('Dropdown', Dropdown);
+app.component('RadioButton', RadioButton);
+app.component('Checkbox', Checkbox);
+app.component('Button', Button);
+
+app.mount('#app');
